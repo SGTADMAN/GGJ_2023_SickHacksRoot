d46e182390b9073762664280dcb7c2b8a5b0a6fe
Adam Lutton <sgtadman@gmail.com>
2023-01-30 23:20:18 +0000

    Added a Skybox. Player moves down the slope (mostly) correctly.

--- Assets/Scripts/DownhillHoverboardScript.cs
+++ Assets/Scripts/DownhillHoverboardScript.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,28 +10,38 @@ public class DownhillHoverboardScript : MonoBehaviour
     [SerializeField] Vector3 input;
     Rigidbody boardRigidbody;
     public LayerMask layerMask;
+    public float multiplier;
+    public float moveForce, turnTorque, leanAmount;
+    public Transform[] anchors = new Transform[4];
+    RaycastHit[] hits = new RaycastHit[4];
+    RaycastHit normalCheckHit;
     [SerializeField] float minimumDistFromGround = 0.5f;
     [SerializeField] Vector3 directionOfTravel;
     [SerializeField] float forwardSpeed = 10;
+    [SerializeField] Transform[] wayPointList;
+    int waypointNo;
+    [SerializeField] Transform targetWaypoint;
+    [SerializeField] Transform lastWaypointPos;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
     private void Start()
     {
         boardRigidbody = GetComponent<Rigidbody>();
         directionOfTravel = transform.forward;
+        waypointNo = 0;
+        targetWaypoint = wayPointList[waypointNo];
+        virtualCamera.LookAt = targetWaypoint;        
     }
     public void HandleMovement(InputAction.CallbackContext context)
     {
         Vector2 rawInput = context.ReadValue<Vector2>();
         input = new Vector3(rawInput.x, 0, rawInput.y);
-    }
-    public float multiplier;
-    public float moveForce, turnTorque;
-    public Transform[] anchors = new Transform[4];
-    RaycastHit[] hits = new RaycastHit[4];
-    RaycastHit normalCheckHit;
+    }  
 
     private void Update()
     {
         boardRigidbody.AddForce(input.x * (moveForce) * transform.right);
+        boardRigidbody.AddTorque(-input.x * (leanAmount) * transform.forward);
     }
     private void FixedUpdate()
     {
@@ -45,11 +56,29 @@ public class DownhillHoverboardScript : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
         }
+
+        if (Vector3.Distance(transform.position, targetWaypoint.position) < Vector3.Distance(lastWaypointPos.position,targetWaypoint.position)/2)
+        {
+            try
+            {
+                waypointNo++;
+                lastWaypointPos = targetWaypoint;
+                targetWaypoint = wayPointList[waypointNo];
+                virtualCamera.LookAt = targetWaypoint;
+            }
+            catch
+            {
+                lastWaypointPos = wayPointList[wayPointList.Length - 2];
+                targetWaypoint = wayPointList[wayPointList.Length-1];
+                virtualCamera.LookAt = targetWaypoint;
+            }
+        }
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.RotateTowards(transform.forward, targetWaypoint.position - transform.position, rotationSpeed * Time.deltaTime, 0.0f), rotationSpeed * Time.deltaTime);
     }
 
     private void ApplyForwardForce()
     {
-        boardRigidbody.AddForce(directionOfTravel * (forwardSpeed),ForceMode.Acceleration);
+        boardRigidbody.AddForce(transform.forward * (forwardSpeed),ForceMode.Acceleration);
     }
 
     void ApplyForce(Transform anchor, RaycastHit hit)
